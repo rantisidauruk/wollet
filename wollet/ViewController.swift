72e57782dc1b0f75a4af62e32f60e941e9751df2
Ranti Sidauruk <rvsidauruk@gmail.com>
2022-04-29 22:16:22 +0700

    Initial Commit

---
+++ wollet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wollet
+//
+//  Created by Ranti Sidauruk on 29/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
